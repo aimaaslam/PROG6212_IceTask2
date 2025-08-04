6a648faca8858870cc6e666bdc32e292a1014f85
aimaaslam <160716330+aimaaslam@users.noreply.github.com>
2025-08-04 13:25:10 +0200

    first commit

---
+++ prjICE_Task_2/UnitTest/Test1.cs
@@ -0,0 +1,47 @@
+﻿using prjICE_Task_2;
+
+namespace UnitTest
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+ 
+        public void testAuthor()
+        {
+            Books books = new Books("Jane Austen", "Emma", "Fiction", 1036, 1815);
+            Assert.AreEqual(books["author"], "Jane Austen");
+        }
+        [TestMethod]
+
+        public void testTitle()
+        {
+            Books books = new Books("Jane Austen", "Emma", "Fiction", 1036, 1815);
+            Assert.AreEqual(books["title"], "Emma");
+        }
+        [TestMethod]
+
+        public void testGenre()
+        {
+            Books books = new Books("Jane Austen", "Emma", "Fiction", 1036, 1815);
+            Assert.AreEqual(books["genre"], "Fiction");
+        }
+        [TestMethod]
+
+        public void updatePages()
+        {
+            Books book = new Books("Jane Austen", "Emma", "Fiction", 1036, 1815);
+            book["pages"] = 1033;
+            Assert.AreEqual(book["pages"], 1033);
+        }
+        [TestMethod]
+        public void updatePublishYear()
+        {
+            Books book = new Books("Jane Austen", "Emma", "Fiction", 1036, 1815);
+            book["year"] = 1816;
+            Assert.AreEqual(book["year"], 1816);
+        }
+        
+
+    }
+}
---
+++ prjICE_Task_2/prjICE_Task_2/Books.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjICE_Task_2
+{
+    public class Books
+    {
+        string bookAuthor, bookTitle, bookGenre;
+        int bookPages, bookPublishYear;
+
+        public Books(string bookAuthor, string bookTitle, string bookGenre, int bookPages, int bookPublishYear)
+        {
+            this.bookAuthor = bookAuthor;
+            this.bookTitle = bookTitle;
+            this.bookGenre = bookGenre;
+            this.bookPages = bookPages;
+            this.bookPublishYear = bookPublishYear;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return this.bookAuthor;
+                else if (index == 1)
+                    return this.bookTitle;
+                else if (index == 2)
+                    return this.bookGenre;
+                else if (index == 3)
+                    return this.bookPages;
+                else if (index == 4)
+                    return this.bookPublishYear;
+                return null;
+            }
+            set
+            {
+                if (index == 0)
+                    this.bookAuthor = (string)value;
+                else if (index == 1)
+                    this.bookTitle = (string)value;
+                else if (index == 2)
+                    this.bookGenre = (string)value;
+                else if (index == 3)
+                    this.bookPages = (int)value;
+                else if (index == 4)
+                    this.bookPublishYear = (int) value;
+            }
+        }
+
+        public object this[string attrName]
+        {
+            get
+            {
+                if (attrName == "author")
+                    return this.bookAuthor;
+                else if (attrName == "title")
+                    return this.bookTitle;
+                else if (attrName == "genre")
+                    return this.bookGenre;
+                else if (attrName == "pages")
+                    return this.bookPages;
+                else if (attrName == "year")
+                    return this.bookPublishYear;
+                return null;
+            }
+            set
+            {
+                if (attrName.ToLower().Equals("author"))
+                    this.bookAuthor = (string)value;
+                else if (attrName.ToLower().Equals("title"))
+                    this.bookTitle = (string)value;
+                else if (attrName.ToLower().Equals("genre"))
+                    this.bookGenre = (string)value;
+                else if (attrName.ToLower().Equals("pages"))
+                    this.bookPages = (int)value;
+                else if (attrName.ToLower().Equals("year"))
+                    this.bookPublishYear = (int)value;
+            }
+        }
+    }
+}
