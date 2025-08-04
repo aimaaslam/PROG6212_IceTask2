34616c025b3fd64439379789867dd034a6c440df
partyhorse42699 <160716029+partyhorse42699@users.noreply.github.com>
2025-08-04 14:51:35 +0200

    Program class shows book info

--- prjICE_Task_2/UnitTest/obj/Debug/net8.0/UnitTest.AssemblyInfo.cs
+++ prjICE_Task_2/UnitTest/obj/Debug/net8.0/UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a648faca8858870cc6e666bdc32e292a1014f85")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- prjICE_Task_2/prjICE_Task_2/Program.cs
+++ prjICE_Task_2/prjICE_Task_2/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Books book = new Books("Jeff Kinney", "Diary Of A Wimpy Kid", "Fiction", 213, 2015);
+
+            book[1] = "Diary Of A Wimpy Kid";
+
+            Console.WriteLine("Author Name: " + book[0]);
+            Console.WriteLine("Book Title: " + book[1]);
+            Console.WriteLine("Book Genre: " + book[2]);
+            Console.WriteLine("Number of pages: " + book[3]);
+            Console.WriteLine("Year book was published: " + book[4]);
+            Console.WriteLine("\n");
+
+            Books book2 = new Books("Stephan Pastis", "Timmy Failure", "Fiction", 202, 2017);
+            book2["bookpublishyear"] = 2017;
+
+            Console.WriteLine("Author Name: " + book2["author"]);
+            Console.WriteLine("Book Title: " + book2["title"]);
+            Console.WriteLine("Book Genre: " + book2["genre"]);
+            Console.WriteLine("Number of pages: " + book2["pages"]);
+            Console.WriteLine("Year book was published: " + book2["year"]);
         }
     }
 }
